2c77c4915139059fbd529d841cca9edd1f7775a6
Yash <yashwalke3434@gmail.com>
2026-08-14 08:05:37 +0530

    Wire BootSequence into Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import { BootSequence } from "../components/BootSequence";
+
 const Index = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      Terminal Component
-    </div>
-  );
+  const [isBooted, setIsBooted] = useState(false);
+
+  if (!isBooted) {
+    return <BootSequence onBootComplete={() => setIsBooted(true)} />;
+  }
+
+  return <div className="h-screen flex items-center justify-center">Terminal goes here (Day 2)</div>;
 };
+
 export default Index;
